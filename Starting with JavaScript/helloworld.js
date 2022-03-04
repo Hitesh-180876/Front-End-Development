948d73e6257171171b51b765d780256bc936b71f
Hitesh Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-03-04 23:26:07 +0530

    till typeof

--- Starting with JavaScript/helloworld.js
+++ Starting with JavaScript/helloworld.js
@@ -1,1 +1,14 @@
+console.log("hello World");
 
+var a =10;
+console.log(a);
+
+console.log(1/0);
+a = 1/0;
+console.log(typeof a);
+console.log(parseInt("aaaa"));
+console.log(Math.sqrt(-10));
+a = null;
+console.log(typeof a);
+console.log(typeof NaN);
+console.log(typeof Infinity);
