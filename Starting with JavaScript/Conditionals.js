eec38da1d8b519a455a3c002b02b6b52cb8c2848
Hitesh Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2022-03-05 23:56:32 +0530

    Conditionals and loops in javascript

---
+++ Starting with JavaScript/Conditionals.js
@@ -0,0 +1,17 @@
+var a=10;
+if(a>0){
+    console.log("Positive");
+}else if(a==0){
+    console.log("Zero");
+}
+else{
+    console.log("Negative");
+}
+
+var b = -1;
+if(b){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
